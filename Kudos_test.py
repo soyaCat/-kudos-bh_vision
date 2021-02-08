41966826e65f9229aeee4a65a23021f7ad47e15b
soya <soyamilk432@gmail.com>
2021-02-08 21:59:22 +0900

    Modify errors 1.파이썬 파일에서 msg를 인식하지 못하는 문제 수정(튜토리얼 보고 약간의 설정이 필요) 2.CMAKEList.txt가 쓸데없는 파일들과 헷갈리게 파일을 만들어대는 것을 정리 3.ReadMD에 패치시 발생문제 대처법에 대해 적어둠 계속 업데이트예정

--- Kudos_test.py
+++ Kudos_test.py
@@ -1,2 +1,24 @@
+"#!/usr/bin/env python"
 import tensorflow as tf
 import cv2
+import numpy as np
+import darknet
+import rospy
+from darknetA.msg import position
+class priROS():
+    def talker(self, posX, posY):
+        pub = rospy.Publisher('visionPos', position, queue_size=10)
+        rospy.init_node('visiontalker', anonymous = True)
+        message = position()
+        message.posX = posX
+        message.posY = posY
+        rospy.loginfo(message)
+        pub.publish(message)
+
+if __name__=='__main__':
+    priROS = priROS()
+    while True:
+        x =  np.random.randn(10,100)
+        posX = 100
+        posY = 50
+        priROS.talker(posX, posY)
---
+++ topic_subcriber.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h" // ROS 기본헤더파일
+#include "darknetA/position.h" // MsgTutorial메시지파일헤더(빌드후자동생성됨)
+// 메시지콜백함수로써, 밑에서설정한ros_tutorial_msg라는이름의토픽
+// 메시지를수신하였을때동작하는함수이다
+// 입력메시지로는ros_tutorials_topic패키지의MsgTutorial메시지를받도록되어있다
+
+void msgCallback(const darknetA::position::ConstPtr& msg)
+{
+ROS_INFO("recievemsg= %d", msg->posX); // stamp.sec메시지를표시한다
+ROS_INFO("recievemsg= %d", msg->posY); // stamp.nsec메시지를표시한다
+}
+
+int main(int argc, char **argv)// 노드메인함수
+{
+ros::init(argc, argv, "topic_subscriber");// 노드명초기화
+ros::NodeHandle nh; // ROS 시스템과통신을위한노드핸들선언
+// 서브스크라이버선언, ros_tutorials_topic패키지의MsgTutorial메시지파일을이용한
+// 서브스크라이버ros_tutorial_sub를작성한다. 토픽명은"ros_tutorial_msg" 이며,
+// 서브스크라이버큐(queue) 사이즈를100개로설정한다는것이다
+ros::Subscriber ros_tutorial_sub= nh.subscribe("visionPos", 100, msgCallback);
+// 콜백함수호출을위한함수로써, 메시지가수신되기를대기, 
+// 수신되었을경우콜백함수를실행한다
+ros::spin();
+
+return 0;
+}
