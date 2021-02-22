6cf0a32e04f8b0096f075158ee5ec9283340b37c
soya <soyamilk432@gmail.com>
2021-02-23 02:08:56 +0900

    add some figure for position.msg

--- kudos_vision.py
+++ kudos_vision.py
@@ -35,37 +35,39 @@ class DataFormatTransfer():
                 if detection[1]>max_confidence:
                     max_confidence = detection[1]
                     bbox = detection[2]
-                    objectCenter = [bbox[2]+(bbox[0]-bbox[2])/2, bbox[3]+(bbox[1]-bbox[3])/2]
+                    objectCenter = [bbox[0], bbox[1]]
+        return objectCenter
+
+    def mapping_point_to_float_shape(self, npArr, objectCenter):
+        im_width_size = np.shape(npArr)[0]
+        im_hight_size = np.shape(npArr)[1]
+        objectCenter[0] = objectCenter[0]/im_width_size
+        objectCenter[1] = objectCenter[1]/im_hight_size
+
         return objectCenter
 
 if __name__=='__main__':
     darknet_config_args = kudos_darknet.parser()
     kudos_darknet.check_arguments_errors(darknet_config_args)
     darknet_network, darknet_class_names, darknet_class_colors, darknet_width, darknet_height = kudos_darknet.Initialize_darknet(darknet_config_args)
-    cap = cv2.VideoCapture(1)
-    #cap = cv2.VideoCapture("test.mp4")
+    #cap = cv2.VideoCapture(-1)
+    cap = cv2.VideoCapture("test.mp4")
     priROS = priROS()
     DataFormatTransfer = DataFormatTransfer()
 
     while True:
         frame, detections = kudos_darknet.getResults_with_darknet(cap, darknet_width, darknet_height, darknet_network, darknet_class_names, darknet_class_colors,darknet_config_args)
-        peopleCenter = DataFormatTransfer.get_one_center_from_detections(detections, label='person')
+        ballCenter = DataFormatTransfer.get_one_center_from_detections(detections, label='ball')
+        print(ballCenter)
+        ballCenter = DataFormatTransfer.mapping_point_to_float_shape(frame, ballCenter)
+        print(ballCenter)
         if np.any(frame) != False:
             cv2.imshow("showIMG", frame)
-        posX = peopleCenter[0]
-        posY = peopleCenter[1]
+        posX = ballCenter[0]
+        posY = ballCenter[1]
         priROS.talker(posX, posY)
         k = cv2.waitKey(1) 
         if k == 27:
             break
     cap.release
     cv2.destroyAllWindows()
-
-
-'''
-    while True:
-        x =  np.random.randn(10,100)
-        posX = 100
-        posY = 50
-        priROS.talker(posX, posY)
-'''
