be57cb8257667ee0060a08996c8dd35d261e73dc
soya <soyamilk432@gmail.com>
2021-03-06 07:14:47 +0900

    Update omo_code

--- omo_send_img.py
+++ omo_send_img.py
@@ -4,7 +4,8 @@ import numpy as np
 import time
 import cv2
 
-if __name__ == "__main__":
+#robot Server!
+if __name__ == '__main__':
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:9010")
@@ -13,11 +14,10 @@ if __name__ == "__main__":
     print("message: ", message)
     cap = cv2.VideoCapture(0)
     while 1:
-        #ret, frame = cap.read()
-        position_list = [0.666,0.999,0.444,0.222]
-        position_npArr = np.array(position_list)
-        print(position_npArr)
-        if np.any(position_npArr) != False:
-            znp.send_array(socket, position_npArr)
-        time.sleep(2)
+        ret, frame = cap.read()
+        np.shape(frame)
+        if np.any(frame) != False:
+            znp.send_array(socket, frame)
+            
+        received_npArr = znp.recv_array(socket)
 
--- omo_yuenou_client.py
+++ omo_yuenou_client.py
@@ -6,7 +6,7 @@ import time
 import cv2
 
 
-host_address = "tcp://localhost:9010"
+host_address = "tcp://localhost:9011"
 
 class DataFormatTransfer():
     def __init__(self):
@@ -57,45 +57,14 @@ def get_socket_and_send_ini_message(host_adress):
     print("Connecting to hello world server...")
     socket = context.socket(zmq.REQ)
     socket.connect(host_address)
-    socket.send(b"hello, this is kudos_vision!")
+    socket.send(b"hello, this is ym_com!")
     return socket
-
-def open_server_and_wait_for_receive_message(host_adress):
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:9011")
-    print("wait receive message from client...")
-    message = socket.recv()
-    print("message: ", message)
-
-    return socket
-
 
 if __name__ == '__main__':
     socket = get_socket_and_send_ini_message(host_address)
-    socket2 = open_server_and_wait_for_receive_message("tcp://*:9011")
-    print("all server open done!")
-    DataFormatTransfer = DataFormatTransfer()
-    darknet_config_args = kudos_darknet.parser()
-    kudos_darknet.check_arguments_errors(darknet_config_args)
-    darknet_network, darknet_class_names, darknet_class_colors, darknet_width, darknet_height = kudos_darknet.Initialize_darknet(darknet_config_args)
-    cap = cv2.VideoCapture(0)
     for request in range(100):
         frame = znp.recv_array(socket)
-        ret = True
-        #ret, frame = cap.read()
-        ballCenter = [-1.0, -1.0]
-        frame, detections = kudos_darknet.getResults_with_darknet(ret, frame, darknet_width, darknet_height, darknet_network, darknet_class_names, darknet_class_colors,darknet_config_args)
-        cv2.imshow("showIMG", frame)
-        ballCenter = DataFormatTransfer.get_one_center_from_detections(detections, label='ball')
-        k = cv2.waitKey(1) 
-        if k == 27:
-            break
+        print(frame)
         omo_send_list = [0.666]
         omo_send_list = np.array(omo_send_list)
         znp.send_array(socket, omo_send_list)
-
-        print(ballCenter)
-        znp.send_array(socket2, omo_send_list)
-        received_npArr = znp.recv_array(socket2)
-
